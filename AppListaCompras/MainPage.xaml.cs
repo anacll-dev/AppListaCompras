75e7a04ac2884f3f14da1f5b41bc39771b437a2a
Usuario <Usuario@CPD03-M14>
2024-03-21 15:06:38 -0300

    editar produto

--- AppListaCompras/MainPage.xaml.cs
+++ AppListaCompras/MainPage.xaml.cs
@@ -25,39 +25,37 @@ namespace AppListaCompras
             DisplayAlert("Somatória", msg, "Fechar");
         }
 
-        protected override void OnAppearing()
+        protected async override void OnAppearing()
         {
             if(lista_produtos.Count == 0)
             {
-                Task.Run(async () =>
-                {
+                 
                     List<Produto> tap = await App.Db.GetAll();
                     foreach (Produto p in tap) 
                     {
                         lista_produtos.Add(p);
                     }
-                });
+          
             }
 
         }
 
-        private void ToolbarItem_Clicked_Add(object sender, EventArgs e)
+        private async void ToolbarItem_Clicked_Add(object sender, EventArgs e)
         {
-        
+            await Navigation.PushAsync(new Views.NovoProduto());
         }
 
-        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
         {
             string q = e.NewTextValue;
             lista_produtos.Clear();
-            Task.Run(async () =>
-            {
+        
                 List<Produto> tap = await App.Db.Search(q);
                 foreach (Produto p in tap)
                 {
                     lista_produtos.Add(p);
                 }
-            });
+          
         }
 
         private void ref_carregando_Refreshing(object sender, EventArgs e)
@@ -65,23 +63,44 @@ namespace AppListaCompras
             lista_produtos.Clear();
             Task.Run(async () =>
             {
-                List<Produto> tap = await App.Db.GetAll();
-                foreach (Produto p in tap)
+                List<Produto> tmp = await App.Db.GetAll();
+                foreach (Produto p in tmp)
                 {
                     lista_produtos.Add(p);
                 }
             });
-            ref_carregando.IsRefreshing = true;
+            ref_carregando.IsRefreshing = false;
         }
 
         private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            Produto? p = e.SelectedItem as Produto;
 
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p
+            });
         }
 
-        private void MenuItem_Clicked_Remover(object sender, EventArgs e)
+        private async void MenuItem_Clicked_Remover(object sender, EventArgs e)
         {
+            try
+            {
+                MenuItem selecionado = (MenuItem)sender;
 
+                Produto p = selecionado.BindingContext as Produto;
+
+                bool confirm = await DisplayAlert("Tem certeza?", "Remover produto?", "Sim", "Cancelar");
+
+                if (confirm)
+                {
+                    await App.Db.Delete(p.Id);
+                    await DisplayAlert("Sucesso!", "Produto Removido", "OK");
+                }
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
--- AppListaCompras/Views/NovoProduto.xaml.cs
+++ AppListaCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,4 @@
+using AppListaCompras.Models;
 namespace AppListaCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +7,25 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Produto Inserido", "OK");
+			await Navigation.PushAsync(new MainPage());
+
+		}catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
